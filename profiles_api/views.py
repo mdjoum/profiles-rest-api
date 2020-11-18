4389d25fda33478102d7e297e05d7da62872d3cf
mdjoum <mdjoum@gmail.com>
2020-11-18 15:05:38 -0500

    enable Django admin for user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.view import Response
 
 # Create your views here.
