4ea4c7ed01eed2b351223135a49d095054253d3f
mdjoum <mdjoum@gmail.com>
2020-11-25 13:37:53 -0500

    added viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
+#viewset
+#configuring url to point to viewset; use a router; different urls for different methods unlike apiviewset
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset') #basename to retrieve url
+
+#apiview set
 urlpatterns=[
-	path('hello-view', views.HelloApiView.as_view())
+	path('hello-view', views.HelloApiView.as_view()),
+	path('', include(router.urls))
 ]
+
+
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,8 @@
 from rest_framework.views import APIView
 from rest_framework.views import Response
 from rest_framework import status
+from rest_framework import viewsets
+
 from profiles_api import serializers #gonna use to tell api view what data to expect whene making post and patch requests
 
 
@@ -62,3 +64,54 @@ class HelloApiView(APIView):
 	def delete(self, request, pk=None):
 		"""Delete an object"""
 		return Response({'method': 'DELETE'})
+
+"""On APIViewSet, you add functions for a particular HTTP method 
+that you want to support on your enpoint. For ViewSet, you add
+functions that represent actions that you would perform on a typical API"""
+
+class HelloViewSet(viewsets.ViewSet):
+	"""Test API ViewSet"""
+	serializer_class=serializers.HelloSerializer
+	"""List is typically a HTTP GET to the root of the enpoint linked 
+	to our viewset. Lists a set of objects that the view point represents"""
+	def list(self, request):
+		"""Return a hello message"""
+		a_viewset=[
+			'Uses actions(list, create, retrieve, update, partial update)', 
+			'Automatically maps to URLs using Routers', 
+			'Provides more functionality with less code',]
+		"""when a request is made to our view set to the root URL of the Viewset,
+		it will call this list function which will create this list here and then it will
+		return it in a response in our API """
+		return Response({'message': 'Hello!', 'a_viewset':a_viewset})
+	#configuring url to point to viewset; use a router; different urls for different methods unlike apiviewset
+
+	def create(self, request):
+		"""Create a new Hello message"""
+		serializer = self.serializer_class(data=request.data)
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message = f'Hello {name}!'
+			return Response({'message':message})
+		else:
+			return Response(
+				serializer.errors,
+				status=status.HTTP_400_BAD_REQUEST)
+
+	def retrieve(self, request, pk=None):
+		"""Handle getting an object by its ID"""
+		return Response({'http_method': 'GET'})
+
+	def update(self, request, pk=None):
+		"""Handle updating an object"""
+		return Response({'http_method':'PUT'})
+
+	def partial_update(self, request, pk=None):
+		"""Handle updating part of an object"""
+		return Response({'http_method': 'PATCH'})
+
+	def destroy(self, request, pk=None):
+		"""Handle removing an object"""
+		return Response({'http_method': 'DELETE'})
+
+
